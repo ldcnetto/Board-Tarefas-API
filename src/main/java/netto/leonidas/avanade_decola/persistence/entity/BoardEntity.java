169b53953ff67595e497263adaf5542227bbc31d
ldcnetto <ldcn@cin.ufpe.br>
2025-03-10 00:28:31 -0300

    feat: criado os entitys

---
+++ src/main/java/netto/leonidas/avanade_decola/persistence/entity/BoardEntity.java
@@ -0,0 +1,35 @@
+package netto.leonidas.avanade_decola.persistence.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static netto.leonidas.avanade_decola.persistence.entity.BoardColumnKindEnum.CANCEL;
+import static netto.leonidas.avanade_decola.persistence.entity.BoardColumnKindEnum.INITIAL;
+
+@Data
+public class BoardEntity {
+    private Long id;
+    private String name;
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private List<BoardColumnEntity> boardColumns = new ArrayList<>();
+
+    public BoardColumnEntity getInitialColumn(){
+        return getFilteredColumn(bc -> bc.getKind().equals(INITIAL));
+    }
+
+    public BoardColumnEntity getCancelColumn(){
+        return getFilteredColumn(bc -> bc.getKind().equals(CANCEL));
+    }
+
+    private BoardColumnEntity getFilteredColumn(Predicate<BoardColumnEntity> filter){
+        return boardColumns.stream()
+                .filter(filter)
+                .findFirst().orElseThrow();
+    }
+}
