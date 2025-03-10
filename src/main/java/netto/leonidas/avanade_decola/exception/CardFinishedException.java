3eefdd1ded4c8660688cb04f259594ada66d6e30
ldcnetto <ldcn@cin.ufpe.br>
2025-03-10 00:30:38 -0300

    feat: criando exceptions

---
+++ src/main/java/netto/leonidas/avanade_decola/exception/CardFinishedException.java
@@ -0,0 +1,8 @@
+package netto.leonidas.avanade_decola.exception;
+
+public class CardFinishedException extends RuntimeException{
+
+    public CardFinishedException(final String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/netto/leonidas/avanade_decola/exception/EntityNotFoundException.java
@@ -0,0 +1,8 @@
+package netto.leonidas.avanade_decola.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
