cbaa8be8d352dbd82fb4841270e3d4e25c2f20d8
ldcnetto <ldcn@cin.ufpe.br>
2025-03-10 00:24:07 -0300

    feat: recriando arquivo

---
+++ src/main/java/netto/leonidas/avanade_decola/AvanadeDecolaApplication.java
@@ -0,0 +1,18 @@
+package netto.leonidas.avanade_decola;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvanadeDecolaApplication {
+
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+        SpringApplication.run(AvanadeDecolaApplication.class, args);
+    }
+
+}
+
