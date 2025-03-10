8b30f5480fb4b8b26fffa54737fef0e218822ec5
ldcnetto <ldcn@cin.ufpe.br>
2025-03-10 00:24:24 -0300

    feat: criando changelogs

---
+++ src/main/resources/db/changelog/migrations/db.changelog-202503100016.sql
@@ -0,0 +1,41 @@
+--liquibase formatted sql
+--changeset ldcnetto:202503100016
+--comment: boards table create
+
+CREATE TABLE BOARDS_COLUMNS(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    `order` int NOT NULL,
+    kind VARCHAR(7) NOT NULL,
+    board_id BIGINT NOT NULL,
+    CONSTRAINT boards__boards_columns_fk FOREIGN KEY (board_id) REFERENCES BOARDS(id) ON DELETE CASCADE,
+    CONSTRAINT id_order_uk UNIQUE KEY unique_board_id_order (board_id, `order`)
+) ENGINE=InnoDB;
+
+--rollback DROP TABLE BOARDS_COLUMNS
+
+CREATE TABLE CARDS(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    board_column_id BIGINT NOT NULL,
+    CONSTRAINT boards_columns__cards_fk FOREIGN KEY (board_column_id) REFERENCES BOARDS_COLUMNS(id) ON DELETE CASCADE
+) ENGINE=InnoDB;
+
+--rollback DROP TABLE CARDS
+
+CREATE TABLE BLOCKS(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    blocked_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    block_reason VARCHAR(255) NOT NULL,
+    unblocked_at TIMESTAMP NULL,
+    unblock_reason VARCHAR(255) NOT NULL,
+    card_id BIGINT NOT NULL,
+    CONSTRAINT cards__blocks_fk FOREIGN KEY (card_id) REFERENCES CARDS(id) ON DELETE CASCADE
+) ENGINE=InnoDB;
+
+--rollback DROP TABLE BLOCKS
+
+ALTER TABLE BLOCKS MODIFY COLUMN unblock_reason VARCHAR(255) NULL;
+
+--rollback ALTER TABLE BLOCKS MODIFY COLUMN unblock_reason VARCHAR(255) NOT NULL;
